75be282f98b5b4d8c13f4897e387c9f25ec95501
voluble-bug-0219 <montensia1@outlook.com>
2019-05-12 22:47:22 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,8 +3,9 @@ def levitation_quiz
 	loop do
 	  puts "What's up, Doc?"
 	  answer = get.chomp
-	  break if answer == "STOP!"
+	  break if answer == "STOP"
 	end
+	puts "Okay, okay, jeez. I'll stop. Sorry."
 end
 
 
